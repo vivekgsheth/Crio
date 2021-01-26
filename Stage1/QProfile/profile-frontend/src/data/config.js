fa0a76898be384316be9e1a8b34c6e8fead48627
Vivek Sheth <37891455+vivekgsheth@users.noreply.github.com>
2021-01-26 20:25:07 +0530

    Add files via upload

---
+++ Stage1/QProfile/profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Vivek Sheth",
+  greeting: "Hey 👋",
+  greetingDescription: "I am skilled Software Engineer who loves to solve practical problems using technology",
+  githubUrl: "https://github.com/vivekgsheth",
+  linkedinUrl: "https://www.linkedin.com/in/vivek-sheth-487aaa157/",
+  cvLink: "https://drive.google.com/file/d/1WGfFMEv9mlE--11QKr32lsBF2T9wMOVT/view?usp=sharing",
+};
